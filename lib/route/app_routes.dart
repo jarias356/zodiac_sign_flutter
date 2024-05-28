eb1d4e42ded4d98dde1dd535e7347c977ec549f8
Solau03 <150872507+Solau03@users.noreply.github.com>
2024-05-28 01:36:50 -0500

    Update app_routes.dart

--- lib/route/app_routes.dart
+++ lib/route/app_routes.dart
@@ -1,10 +1,14 @@
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:zodiac_sign_flutter/route/route_const.dart';
+import 'package:zodiac_sign_flutter/ui/getcompatibility/get_compatibility_screen.dart';
 import 'package:zodiac_sign_flutter/ui/login/login_screen.dart';
+import 'package:zodiac_sign_flutter/ui/getsign/get_sign_screen.dart';
 
 class AppRoutes {
   static final List<GetPage<dynamic>> routes = [
     GetPage(name: RouteConst.loginPage, page: () => LoginScreen()),
+    GetPage(name: RouteConst.getPages, page: () => const GetSign()),
+    GetPage(name: RouteConst.compatibilityPages, page: () =>  const GetCompatibility ()),
   ];
 
 }
