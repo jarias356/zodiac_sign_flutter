6e748fb63ab9076e48b7c1f3758ddd29e7d65b6a
Solau03 <150872507+Solau03@users.noreply.github.com>
2024-05-29 03:55:58 -0500

    Update route_const.dart

--- lib/route/route_const.dart
+++ lib/route/route_const.dart
@@ -2,4 +2,5 @@ class RouteConst {
   static String  loginPage = "/login";
   static String  getPages = "/get";
   static String  compatibilityPages= "/compatibility";
+  static String  moreGetPages= "/morget";
 }
